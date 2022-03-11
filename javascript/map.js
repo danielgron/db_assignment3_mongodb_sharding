fc540f1eb5dca5bde72d6243c434d589f3dff2c6
Benjo6 <58556176+Benjo6@users.noreply.github.com>
2022-03-11 11:06:40 +0100

    Update map.js

--- javascript/map.js
+++ javascript/map.js
@@ -1,11 +1,14 @@
 map = function()
 {
-var names = distinctName(this);
-emit({
- names: names,
-follower_count: this.components.users.follower_count
-},{
-count: 1
-});
+ var names = distinctName(this);
+ emit
+ (
+  {
+   names: names,
+   follower_count: this.components.users.follower_count
+  },{
+   count: 1
+  }
+ );
 }
 
