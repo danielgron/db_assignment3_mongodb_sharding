8f6799c3e084d5e516f9ec0f0a82bd072315c00f
Daniel Grønbjerg <danielgronbjerg@gmail.com>
2022-03-16 15:32:11 +0100

    Add data

--- tweet-frontend/src/app/_components/tweet-list/tweet-list.component.ts
+++ tweet-frontend/src/app/_components/tweet-list/tweet-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { Tweet } from 'src/app/tweet';
 import { TweetService } from 'src/app/_services/tweet.service';
 
@@ -14,14 +14,27 @@ export class TweetListComponent implements OnInit {
   sh: number | undefined
   isChecked: boolean = false
 
-  name = new FormControl('');
+  tweetForm!: FormGroup;
 
-  constructor(private tweetService: TweetService) {
+  constructor(private tweetService: TweetService, private fb: FormBuilder) {
 
   }
 
   ngOnInit(): void {
     this.tweetService.getTweets().subscribe(x => this.tweets = x);
+
+    this.tweetForm = this.fb.group({
+      source: new FormControl(''),
+      email: new FormControl(''),
+      text: new FormControl('')
+    });
+  }
+
+  onSubmit(form: FormGroup) {
+    console.log('Valid?', form.valid); // true or false
+    console.log('Source', form.value.source);
+    console.log('Email', form.value.email);
+    console.log('Text', form.value.text);
   }
 
 }
