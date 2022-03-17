df76e867c9bbf954d14868fe071ee924ee868c9a
Daniel Grønbjerg <danielgronbjerg@gmail.com>
2022-03-17 14:11:44 +0100

    remove extra quotes, add ipbind to compose

--- config-setup-1.sh
+++ config-setup-1.sh
@@ -11,7 +11,7 @@ sudo fallocate -l 2G /swapfile
 sudo chmod 600 /swapfile
 sudo mkswap /swapfile
 sudo swapon /swapfile
-sudo echo \"\n/swapfile swap swap defaults 0 0\" >> /etc/fstab
+sudo echo "\n/swapfile swap swap defaults 0 0" >> /etc/fstab
 
 sudo mkdir /docker
 sudo mkdir /docker/data
@@ -21,17 +21,17 @@ curl https://raw.githubusercontent.com/danielgron/db_assignment3_mongodb_shardin
 curl https://raw.githubusercontent.com/danielgron/db_assignment3_mongodb_sharding/main/twitter.json -o /docker/data/twitter.json
 curl https://raw.githubusercontent.com/danielgron/db_assignment3_mongodb_sharding/main/tweets.bson -o /docker/data/twitter.bson
 
-sudo curl -L \"https://github.com/docker/compose/releases/download/1.29.2/docker-compose-Linux-x86_64\" -o /usr/local/bin/docker-compose 
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-Linux-x86_64" -o /usr/local/bin/docker-compose 
 sudo chmod +x /usr/local/bin/docker-compose
 
 sudo usermod -aG docker $USER
-newgrp docker 
+newgrp docker
 sudo chgrp docker /usr/local/bin/docker-compose
 docker-compose -f /docker/docker-compose.yml up -d
 
 
 docker exec mongocfg1 bash -c "echo 'rs.initiate({_id: \"mongors1conf\",configsvr: true, members: [{ _id : 0, host : \"mongocfg1\" },{ _id : 1, host : \"mongocfg2\" }, { _id : 2, host : \"mongocfg3\" }]})' | mongosh --quiet"
-docker exec -it mongocfg1 bash -c "echo 'rs.status()' | mongosh --quiet"
+docker exec mongocfg1 bash -c "echo 'rs.status()' | mongosh --quiet"
 
 
 
